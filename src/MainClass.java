c5c2cb6e405684565c7de84e02d8f8b72a368d21
Your Name <you@example.com>
2018-12-10 14:56:41 +0300

    ikinci kayit

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,9 +1,14 @@
 
 public class MainClass {
 
+			private static int denemeSayisi = 25;
+			public String denemeYazisi = "Bu Bir Deneme Yazisidir.";
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		denemeSayisi += 9;
+		
 	}
 
 }
